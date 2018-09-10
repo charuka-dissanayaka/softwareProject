406b318c9cbbf3bf9a7e58758fb4afd24536eb20
Buddhika Chathuranga <vmbckscc456@gmail.com>
2018-09-11 01:03:53 +0530

    dashboard updated

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TutorCardListService } from '../services/tutor-card-list.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  tutors;
+  count = 0;
+
+  constructor(private tutorListService: TutorCardListService) { 
+    this.scrapTutors();
+  }
 
   ngOnInit() {
+  }
+
+
+  disCount(dis){
+    let count = 0;
+    for(let i of this.tutors){
+      if(i.location == dis.value){
+        count++;
+      }
+    }
+    this.count = count;
+  }
+
+  scrapTutors() {
+    let tutorList = this.tutorListService.getTutorList()
+      .subscribe(data => {
+        //console.log(data.json().user);
+        console.log("constructor");
+        this.tutors = data.json().user;
+      });
   }
 
 }
